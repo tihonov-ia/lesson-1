2e75fe3198c1cda7dc603435ef2a756aca0db2fc
tihonov-ia <tia22@yandex.ru>
2024-06-18 23:57:08 +0300

    Add files via upload

---
+++ mainn.py
@@ -0,0 +1,26 @@
+print("Hello World")
+# Задача 1
+print(len('a'))
+# Задача 2
+first = 5
+second = 3
+print(first + second, first - second )
+summa = 8
+dif = 2
+print(summa, dif)
+# Задача 3
+first = 1
+second = 2
+thride = 3
+mean = (first + second + thride) / 3
+print(mean)
+# Задача 4
+first_string = ("Вторник")
+second_string = ("Понедельник,")
+print(second_string, first_string)
+# Задача 5
+a = 1
+b = 2
+c = 3
+f=(((a * b) + (a * c))**3)/2
+print(f)
